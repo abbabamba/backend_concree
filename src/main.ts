66e7022a711a04996d193125f4cb40b64fa0c162
abbabamba <babacarbamba11@gmail.com>
2024-09-02 06:19:21 +0000

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Activer CORS pour permettre les requêtes depuis votre frontend
   await app.listen(3001);
 }
 bootstrap();
